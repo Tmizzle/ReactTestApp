d8c8f7f774d42a91166d0dcdd1e555d7c1088bd7
Tmizzle <aleksandarplanic21@gmail.com>
2022-12-06 09:15:31 +0100

    Added state property to MainMenu

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import './App.css';
 import { Container} from 'react-bootstrap';
-import { faHome, faUpload} from '@fortawesome/free-solid-svg-icons';
+import { faHome} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { MainMenu } from '../MainMenu/MainMenu';
 
 function App() {
   return (
