2cfb6c73dac2c2e25f21872bcfe6d0ad9b07cf5b
Tmizzle <aleksandarplanic21@gmail.com>
2022-12-06 08:42:47 +0100

    Created MainMenu component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,12 +3,12 @@ import './App.css';
 import { Container} from 'react-bootstrap';
 import { faHome, faUpload} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { MainMenu } from '../MainMenu/MainMenu';
 
 function App() {
   return (
     <Container>
       <FontAwesomeIcon icon={faHome}/> Home
-      <FontAwesomeIcon icon={faUpload}/> Upload
     </Container>
   );
 }
